2d6b743ba9e7c1520570f5a720c0e6724cacefeb
edu <eduar766@gmail.com>
2023-04-24 10:00:52 -0400

    initial app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/screens/screens.dart';
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movies App',
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => const HomeScreen(),
+        'details': (context) => const DetailsScreen()
+      },
+    );
+  }
+}
